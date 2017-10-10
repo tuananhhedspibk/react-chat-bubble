0f8675357a127dfef3788eba859d7a9179dae172
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-10-11 00:04:52 +0700

    change index file

--- src/index.js
+++ src/index.js
@@ -2,45 +2,24 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ChatBubble from "./components/ChatBubble";
 
-
 const image = 'http://www.bradfordwhite.com/sites/default/files/images/corporate_imgs/iStock_000012107870XSmall.jpg';
 
-class App extends Component {
-  state = {
-    messages: [
-      {
-        type: 0,
-        image,
-        text: "Hello! Good Morning!"
-      },
-      {
-        type: 1,
-        image,
-        text: "Hello! Good Afternoon!"
-      }
-    ],
-  };
-
-  handleNewMessage = text =>
-    this.setState({
-      messages: this.state.messages.concat([{
-        text,
-        type: 0,
-        image,
-      }])
-    });
-
-  render() {
-    return (
-      <ChatBubble
-        messages={this.state.messages}
-        onNewMessage={this.handleNewMessage}
-      />
-    );
+let messages = [
+  {
+    type: 0,
+    image,
+    text: "Hello! Good Morning!",
+    time: '15:00'
+  },
+  {
+    type: 1,
+    image,
+    text: "Hello! Good Afternoon!",
+    time: '14:00'
   }
-}
+];
 
 ReactDOM.render(
-  <App/>,
+  <ChatBubble messages={messages}/>,
   document.getElementById('root')
 );
